ea6621024870b9a52051c17c2c7e2caf864af9cc
boh001 <boh001g@gmail.com>
2020-03-07 12:18:22 +0900

    2020-03-07

--- src/Components/Clock/Clock.style.js
+++ src/Components/Clock/Clock.style.js
@@ -8,12 +8,14 @@ export const Frame = styled.div`
   flex-direction: column;
   font-size: 70px;
   font-weight: bold;
-  background-image: url(${props => `images/${props.src}.jpg`});
-  /* background-image: url(${props => `images/thunderstorm.jpg`}); */
+  /* background-image: url(${props => `images/${props.src}.jpg`}); */
+  background-image: url(${props =>
+    `https://images.unsplash.com/photo-1583356016310-e6db1eb14165?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80`});
   background-size: cover;
   color: white;
   @media screen and (max-width: 1100px) {
     font-size: 40px;
+    /* background-size:1100px; */
   }
 `;
 export const Clock = styled.div``;
